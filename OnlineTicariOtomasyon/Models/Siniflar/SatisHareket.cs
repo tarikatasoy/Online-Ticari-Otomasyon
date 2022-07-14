ed36ff4c0bb75f9b3dd5dfeb6ef8b7744f58570e
tarikatasoy <50027672+tarikatasoy@users.noreply.github.com>
2022-07-14 18:43:41 +0300

    Proje dosyası ekle.

---
+++ OnlineTicariOtomasyon/Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int Satisid { get; set; }
+        //ürün
+        //cari
+        //personel
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public Urun Urun { get; set; }
+        public Cariler Cariler { get; set; }
+        public Personel Personel { get; set; }
+        
+
+    }
+}
